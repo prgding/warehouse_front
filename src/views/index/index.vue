abe21f8680271b19be3edc3e10e9b0f71523218d
dings <1203823603@qq.com>
2024-03-31 18:33:27 +0800

    change /home

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,17 +1,120 @@
 <template>
-  <div>
-    <el-image
-        src="./index.jpg"
-        style="width: 81%;"
-    ></el-image>
-  </div>
+  <el-row :gutter="16">
+    <el-col :span="8">
+      <div class="statistic-card">
+        <el-statistic :value="98500">
+          <template #title>
+            <div class="div">
+              今日采购量
+            </div>
+          </template>
+        </el-statistic>
+        <div class="statistic-footer">
+          <div class="footer-item">
+            <span class="span">相比昨日</span>
+            <span class="green">
+              24%
+              <el-icon>
+                <CaretTop/>
+              </el-icon>
+            </span>
+          </div>
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="8">
+      <div class="statistic-card">
+        <el-statistic :value="693700">
+          <template #title>
+            <div class="div">
+              今日入库量
+            </div>
+          </template>
+        </el-statistic>
+        <div class="statistic-footer">
+          <div class="footer-item">
+            <span class="span">相比昨日</span>
+            <span class="red">
+              12%
+              <el-icon>
+                <CaretBottom/>
+              </el-icon>
+            </span>
+          </div>
+        </div>
+      </div>
+    </el-col>
+    <el-col :span="8">
+      <div class="statistic-card">
+        <el-statistic :value="72000" title="New transactions today">
+          <template #title>
+            <div class="div">
+              今日出库量
+            </div>
+          </template>
+        </el-statistic>
+        <div class="statistic-footer">
+          <div class="footer-item">
+            <span class="span">相比昨日</span>
+            <span class="green">
+              16%
+              <el-icon>
+                <CaretTop/>
+              </el-icon>
+            </span>
+          </div>
+        </div>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
-<script setup>
-import {ref} from 'vue'
-import {useRouter} from 'vue-router'
-
+<script lang="ts" setup>
+import {CaretBottom, CaretTop,} from '@element-plus/icons-vue'
 </script>
+
 <style scoped>
+.statistic-card {
+  height: 100%;
+  padding: 20px;
+  border-radius: 4px;
+  background-color: var(--el-bg-color-overlay);
+}
+
+.statistic-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  font-size: 15px;
+  color: var(--el-text-color-regular);
+  margin-top: 16px;
+}
+
+.statistic-footer .footer-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.statistic-footer .footer-item span:last-child {
+  display: inline-flex;
+  align-items: center;
+  margin-left: 4px;
+}
+
+.green {
+  color: var(--el-color-success);
+}
+
+.red {
+  color: var(--el-color-error);
+}
+
+.div {
+  display: inline-flex;
+  align-items: center;
+  font-size: 20px;
+}
 
 </style>
