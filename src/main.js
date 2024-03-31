116ecd612fdcc7ff1fdc16d59b2df6670b501cbb
dings <1203823603@qq.com>
2024-03-31 18:34:03 +0800

    update element plus

--- src/main.js
+++ src/main.js
@@ -30,7 +30,6 @@ import loading from "@/common/sureLoading"
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-
 import App from './App.vue'
 
 
